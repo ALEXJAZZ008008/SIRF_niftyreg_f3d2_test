b84c9e6b3df8558670579c7846e53aae08953285
ALEXJAZZ008008 <alexandercwhitehead@gmail.com>
2021-06-10 18:31:45 +0100

    initial commit

---
+++ affine/main.py
@@ -0,0 +1,35 @@
+# Copyright University College London 2021
+# Author: Alexander Whitehead, Institute of Nuclear Medicine, UCL
+# For internal research only.
+
+
+import sirf.Reg as reg
+
+
+nifty_f3d_sym = reg.NiftyF3dSym()
+
+nifty_f3d_sym.set_reference_image(reg.NiftiImageData3D("./ref.nii"))
+nifty_f3d_sym.set_floating_image(reg.NiftiImageData3D("./flo.nii"))
+
+nifty_aladin_sym = reg.NiftyAladinSym()
+
+nifty_aladin_sym.set_reference_image(reg.NiftiImageData3D("./ref.nii"))
+nifty_aladin_sym.set_floating_image(reg.NiftiImageData3D("./flo.nii"))
+
+nifty_aladin_sym.process()
+
+nifty_f3d_sym.set_initial_affine_transformation(nifty_aladin_sym.get_transformation_matrix_forward())
+
+nifty_f3d_sym.process()
+
+resampler = reg.NiftyResample()
+
+nifty_f3d_sym.get_deformation_field_forward().write("./dvf.nii")
+
+resampler.set_reference_image(reg.NiftiImageData3D("./ref.nii"))
+resampler.set_floating_image(reg.NiftiImageData3D("./flo.nii"))
+resampler.add_transformation(nifty_f3d_sym.get_deformation_field_forward())
+
+resampler.set_interpolation_type_to_cubic_spline()
+
+resampler.forward(reg.NiftiImageData3D("./flo.nii")).write("./output.nii")
